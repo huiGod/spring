998cc85ae3cfb482173adeef5de8d0105853267c
huigod <tenghuiit@163.com>
2022-02-16 09:17:30 +0800

    update doc

--- spring-aop/src/main/java/org/springframework/aop/framework/ReflectiveMethodInvocation.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/ReflectiveMethodInvocation.java
@@ -159,10 +159,13 @@ public class ReflectiveMethodInvocation implements ProxyMethodInvocation, Clonea
 	@Nullable
 	public Object proceed() throws Throwable {
 		//	We start with an index of -1 and increment early.
+		// 拦截器链中的最后一个拦截器执行完后，即可执行目标方法
 		if (this.currentInterceptorIndex == this.interceptorsAndDynamicMethodMatchers.size() - 1) {
+			// 执行目标方法
 			return invokeJoinpoint();
 		}
 
+		//currentInterceptorIndex为-1，每次调用都递增下一个拦截器
 		Object interceptorOrInterceptionAdvice =
 				this.interceptorsAndDynamicMethodMatchers.get(++this.currentInterceptorIndex);
 		if (interceptorOrInterceptionAdvice instanceof InterceptorAndDynamicMethodMatcher) {
@@ -170,18 +173,22 @@ public class ReflectiveMethodInvocation implements ProxyMethodInvocation, Clonea
 			// been evaluated and found to match.
 			InterceptorAndDynamicMethodMatcher dm =
 					(InterceptorAndDynamicMethodMatcher) interceptorOrInterceptionAdvice;
+			//匹配目标方法
 			if (dm.methodMatcher.matches(this.method, this.targetClass, this.arguments)) {
+				// 调用拦截器逻辑
 				return dm.interceptor.invoke(this);
 			}
 			else {
 				// Dynamic matching failed.
 				// Skip this interceptor and invoke the next in the chain.
+				// 如果匹配失败，则忽略当前的拦截器，调用下一个拦截器
 				return proceed();
 			}
 		}
 		else {
 			// It's an interceptor, so we just invoke it: The pointcut will have
 			// been evaluated statically before this object was constructed.
+			// 调用拦截器逻辑，并传递 ReflectiveMethodInvocation 对象
 			return ((MethodInterceptor) interceptorOrInterceptionAdvice).invoke(this);
 		}
 	}
